ad187c1d91ce955b9c1e99fe26f09aa9d2b4ae3f
arthurtruong <arthurtruong.work@gmail.com>
2018-05-18 18:56:01 +1000

    added css with bootstrap, built navigation bar, built crypto controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @hello = "hello how are you jessie"
+
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    @uri = URI(@url)
+    @response  = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
   end
 
   def about
