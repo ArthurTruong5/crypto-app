6d0c706305b3e65eb4290bd3df6ea2087e40aff4
arthurtruong <arthurtruong.work@gmail.com>
2018-05-19 16:04:28 +1000

    Created crypto output

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
-
     require 'net/http'
     require 'json'
 
-    @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
     @uri = URI(@url)
     @response  = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM",]
   end
 
   def about
