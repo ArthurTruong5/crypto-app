75a36a305639de6923422b1486528cbf2e9ae1d8
arthurtruong <arthurtruong.work@gmail.com>
2018-05-20 13:48:30 +1000

    added error handling for current_user.id and added a new link in navbar called add new and list crypto

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,6 +1,7 @@
 class CurrenciesController < ApplicationController
   before_action :set_currency, only: [:show, :edit, :update, :destroy]
-  
+  before_action :authenticate_user!
+
 
   # GET /currencies
   # GET /currencies.json
